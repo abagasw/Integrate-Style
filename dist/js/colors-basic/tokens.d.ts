43ca61b8dcc1d6ebb91764eb8fddac5c42b41cbf
abagasw <alit2bagas@gmail.com>
2025-08-15 23:36:55 +0700

    Add generated token files to dist folder

---
+++ dist/js/colors-basic/tokens.d.ts
@@ -0,0 +1,45 @@
+// Generated design token types
+export interface DesignTokens {
+  primary: {
+    value: string;
+    type: 'color';
+  };
+  secondary: {
+    value: string;
+    type: 'color';
+  };
+  white: {
+    value: string;
+    type: 'color';
+  };
+  black: {
+    value: string;
+    type: 'color';
+  };
+  gray: {
+    50: {
+      value: string;
+      type: 'color';
+    };
+    100: {
+      value: string;
+      type: 'color';
+    };
+    200: {
+      value: string;
+      type: 'color';
+    };
+    500: {
+      value: string;
+      type: 'color';
+    };
+    900: {
+      value: string;
+      type: 'color';
+    };
+  };
+
+}
+
+export declare const tokens: DesignTokens;
+export default tokens;
---
+++ dist/js/spacing-basic/tokens.js
@@ -0,0 +1,16 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 15 Aug 2025 16:36:30 GMT
+ */
+
+export const SpacingXs = "4px";
+export const SpacingSm = "8px";
+export const SpacingMd = "16px";
+export const SpacingLg = "24px";
+export const SpacingXl = "32px";
+export const Spacing2xl = "48px";
+export const BorderRadiusNone = "0px";
+export const BorderRadiusSm = "4px";
+export const BorderRadiusMd = "8px";
+export const BorderRadiusLg = "12px";
+export const BorderRadiusFull = "9999px";
